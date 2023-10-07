4e94de005257ff158b72c04861da66ec5658ce04
victor-imani <91277156+victor-imani@users.noreply.github.com>
2023-10-07 07:32:13 -0400

    home page moved

--- lib/pages/nav_pages/main_page.dart
+++ lib/pages/nav_pages/main_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:trips/pages/nav_pages/bar_item_page.dart';
-import 'package:trips/pages/nav_pages/home_page.dart';
+import 'package:trips/pages/home_page.dart';
 import 'package:trips/pages/nav_pages/my_page.dart';
 import 'package:trips/pages/nav_pages/search_page.dart';
 
